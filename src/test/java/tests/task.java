577c9c9acaa692463bdf7c44c76a4e8eb7394610
cybertek <github@cybertekschool.com>
2019-12-09 13:15:32 -0500

    add void method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -5,8 +5,10 @@ public class task {
  public static void main(String[] args){
  System.out.println("Hello world");
   //this line is added from GitHub, see you in IntelliJ
-  
- }
+  }
 
+ public void hi(){
+     System.out.println("Hi from IntelliJ");
+    }
 
  }
