d47cd44382d63d4aacfd4c5b2e3b27417f892dc4
CybertekSchool <33201481+CybertekSchool@users.noreply.github.com>
2019-12-09 12:37:46 -0500

    updated task class

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,6 +1,12 @@
 package tests;
 
 public class task {
+ 
+ public static void main(String[] args){
+ System.out.println("Hello world");
+  //this line is added from GitHub, see you in IntelliJ
+  
+ }
 
 
  }
